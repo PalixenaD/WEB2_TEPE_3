d73a46ef8bbe8a6ea836900a85000e8181051e0d
joaquinaranzadi <aranzadijoaquin@gmail.com>
2026-06-15 17:56:47 -0300

    Update api_router.php

--- api_router.php
+++ api_router.php
@@ -1,26 +1,27 @@
 <?php
 require_once 'libs/router/router.php';
-require_once 'app/controllers/issues-api.controller.php';
+require_once 'app/controllers/album-api.controller.php';
+require_once 'app/controllers/artista-api.controller.php';
 
 
 $router = new Router();
 
 // ALBUMES
-$router->addRoute('issues', 'GET', 'IssuesApiController', 'getIssues');
-$router->addRoute('issues/:id', 'GET', 'IssuesApiController', 'getIssueById');
-$router->addRoute('issues/:id', 'DELETE', 'IssuesApiController', 'removeIssue');
-$router->addRoute('issues', 'POST', 'IssuesApiController', 'insertIssue');
-$router->addRoute('issues/:id', 'PUT', 'IssuesApiController', 'updateIssue');
-$router->addRoute('issues/:id', 'PATCH', 'IssuesApiController', 'patchIssue');
+$router->addRoute('album', 'GET', 'AlbumApiController', 'getAlbums');
+$router->addRoute('album/:id', 'GET', 'AlbumApiController', 'getAlbumById');
+$router->addRoute('album/:id', 'DELETE', 'AlbumApiController', 'removeAlbum');
+$router->addRoute('album', 'POST', 'AlbumApiController', 'insertAlbum');
+$router->addRoute('album/:id', 'PUT', 'AlbumApiController', 'updateAlbum');
+$router->addRoute('album/:id', 'PATCH', 'AlbumApiController', 'patchAlbum');
 
 
 //ARTISTA
-$router->addRoute('issues', 'GET', 'IssuesApiController', 'getIssues');
-$router->addRoute('issues/:id', 'GET', 'IssuesApiController', 'getIssueById');
-$router->addRoute('issues/:id', 'DELETE', 'IssuesApiController', 'removeIssue');
-$router->addRoute('issues', 'POST', 'IssuesApiController', 'insertIssue');
-$router->addRoute('issues/:id', 'PUT', 'IssuesApiController', 'updateIssue');
-$router->addRoute('issues/:id', 'PATCH', 'IssuesApiController', 'patchIssue');
+$router->addRoute('artista', 'GET', 'ArtistaApiController', 'getArtistas');
+$router->addRoute('artista/:id', 'GET', 'ArtistaApiController', 'getArtistaById');
+$router->addRoute('artista/:id', 'DELETE', 'ArtistaApiController', 'removeArtista');
+$router->addRoute('artista', 'POST', 'ArtistaApiController', 'insertArtista');
+$router->addRoute('artista/:id', 'PUT', 'ArtistaApiController', 'updateArtista');
+$router->addRoute('artista/:id', 'PATCH', 'ArtistaApiController', 'patchArtista');
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
 
