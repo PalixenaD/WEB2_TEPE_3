914a7d0742d4fffcd052afba62484d852ef703da
PalixenaD <palixenad2@gmail.com>
2026-06-15 18:06:12 -0300

    Cree artista-api.controller.php

--- app/controllers/artista-api.controller.php
+++ app/controllers/artista-api.controller.php
@@ -1,1 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../models/artista-api.model.php';
+
+class ArtistaApiController {
+
+    private $model;
+
+    public function __construct() {
+        $this->model = new ArtistaModel();
+    }
 
