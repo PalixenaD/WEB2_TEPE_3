48795286db00990ae1e0628612dc4f03994a6cdd
joaquinaranzadi <aranzadijoaquin@gmail.com>
2026-06-16 18:06:44 -0300

    Update api_router.php

--- api_router.php
+++ api_router.php
@@ -1,25 +1,22 @@
 <?php
 require_once 'libs/router/router.php';
 require_once 'app/controllers/album-api.controller.php';
-require_once 'app/controllers/artista-api.controller.php';
+require_once 'app/controllers/reseña-api.controller.php';
 
 
 $router = new Router();
 
 // ALBUMES
-$router->addRoute('album', 'GET', 'AlbumApiController', 'getAlbums');
-$router->addRoute('album/:id', 'GET', 'AlbumApiController', 'getAlbumById');
-$router->addRoute('album/:id', 'DELETE', 'AlbumApiController', 'removeAlbum');
-$router->addRoute('album', 'POST', 'AlbumApiController', 'insertAlbum');
-$router->addRoute('album/:id', 'PUT', 'AlbumApiController', 'updateAlbum');
+$router->addRoute('albums', 'GET', 'AlbumApiController', 'getAlbums');
+$router->addRoute('albums/:id', 'GET', 'AlbumApiController', 'getAlbumById');
+$router->addRoute('albums/:id', 'DELETE', 'AlbumApiController', 'removeAlbum');
+$router->addRoute('albums', 'POST', 'AlbumApiController', 'insertAlbum');
+$router->addRoute('albums/:id', 'PUT', 'AlbumApiController', 'updateAlbum');
 
 
 //ARTISTA
-$router->addRoute('artista', 'GET', 'ArtistaApiController', 'getArtistas');
-$router->addRoute('artista/:id', 'GET', 'ArtistaApiController', 'getArtistaById');
-$router->addRoute('artista/:id', 'DELETE', 'ArtistaApiController', 'removeArtista');
-$router->addRoute('artista', 'POST', 'ArtistaApiController', 'insertArtista');
-$router->addRoute('artista/:id', 'PUT', 'ArtistaApiController', 'updateArtista');
+$router->addRoute('albums/:id/reseñas', 'GET', 'ReseñaApiController', 'getReseñasByAlbum');
+$router->addRoute('reseñas', 'POST', 'ReseñaApiController', 'insertReseña');
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
 
